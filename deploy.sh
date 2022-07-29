2da1b89dcbe65b3198b5a0e7f273d1abb8d291fb
geethanjali131 <muralimohangeetha13@gmail.com>
2022-07-29 10:25:51 +0530

    merge conflict fixed..

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
-
 hello am  updating this line from master branch
 updating this line from dev branch   development
 updating dev branch....
-
-
 the file contains deployment script
- updating manually in github gui
+ updating manually githun gui
